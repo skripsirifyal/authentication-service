82bcf1f62d2241cbbb96d77a217a15ae5eeb3e8a
Moh Rifyal Azmi <skripsirifyal@gmail.com>
2022-08-21 20:36:06 +0700

    Initial commit

---
+++ src/test/java/id/ac/upiyai/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.upiyai.authenticationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
